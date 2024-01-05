7e5318cd58c135d02e42d55b7599715d31839259
Jaggishah <jugdeepshah@gmail.com>
2024-01-05 16:36:41 -0700

    learning Java:

---
+++ intro.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class jaggishah {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter The Data:");
+        int jaggi = sc.nextInt();
+
+        System.out.println("jaggi");
+        System.out.println(jaggi);
+        sc.close();
+    }
+}
