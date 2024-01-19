dcba25189306c364dfc826b5b98d53c93199052f
Jaggishah <jugdeepshah@gmail.com>
2024-01-19 01:04:56 -0700

    moreloops

---
+++ moreloops/diamondstar.java
@@ -0,0 +1,22 @@
+class diamondstar{
+    public static void main(String[] args){
+        int N = 5;
+        int intialstar = 1;
+        for (int rows = 1 ; rows <= N ; rows++){
+             for ( int stars = intialstar ; stars >= 1 ; stars--){
+                System.out.print("*");
+             }
+                intialstar += 2;
+                System.out.println(" ");
+        }
+        intialstar -= 4;
+        for (int rows = N; rows >= 1 ; rows--){
+             for ( int stars = intialstar ; stars >= 1 ; stars--){
+                System.out.print("*");
+             }
+                intialstar -= 2;
+                System.out.println(" ");
+        }
+      
+    }
+}
---
+++ moreloops/printingstars.java
@@ -0,0 +1,15 @@
+class printStar{
+    public static void main(String[] args){
+        int N = 4; 
+        int R = N;
+        for(;N>=1;N--){
+        
+            for (int j = R ; j>=1 ; j--){
+                System.out.print('*');
+                }
+                System.out.println();
+            }
+
+            
+    }
+}
