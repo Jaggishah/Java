a9da5c244e536a0043757416b48bb61a123dd05a
Jaggishah <jugdeepshah@gmail.com>
2024-01-26 23:09:32 -0700

    spiral

--- muitldimensional/spiralorder.java
+++ muitldimensional/spiralorder.java
@@ -10,8 +10,39 @@ class diagonal{
         int R = matrix.length ; 
         int C = matrix[0].length ; 
 
-        System.out.print("Row = > " R + "Col =>" + C) ;
+        System.out.print( R + " " + C);
+        int top = 0 , left = 0 , bottom = R - 1 , right = C - 1;
 
+        while( top <= bottom && left <= right){
+            System.out.println("");
+            for (int i = left ; i <= right ; i ++){
+                System.out.print(matrix[top][i] + " ");
+            }
+
+            System.out.println("");
+            for (int i = top + 1 ; i <= bottom ; i ++){
+                System.out.print(matrix[i][right] + " ");
+            }
+
+            System.out.println("");
+            for (int i = right  ; i >= left ; i--){
+                System.out.print(matrix[bottom][i] + " ");
+            }
+
+            System.out.println("");
+            for (int i = right-1 ; i >= left ; i--){
+                System.out.print(matrix[bottom][i] + " ");
+            }
+
+            System.out.println("");
+            for (int i = bottom - 1 ; i >= top ; i--){
+                System.out.print(matrix[i][left] + " ");
+            }
+
+        top++; left++;
+
+        bottom--; right--;
+        }
 
     }
 }
